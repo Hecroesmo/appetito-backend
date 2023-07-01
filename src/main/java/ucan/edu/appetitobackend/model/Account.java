08332d7304b680654ba4ac30203cb9388c2e6396
Hecroesmo <hecroesmo.jesus77@outlook.com>
2023-07-01 12:12:05 +0100

    adicionados parametros de construtor lumbok

--- src/main/java/ucan/edu/appetitobackend/model/Account.java
+++ src/main/java/ucan/edu/appetitobackend/model/Account.java
@@ -11,9 +11,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Account implements Serializable {
 
